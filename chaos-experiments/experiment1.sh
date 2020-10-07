25ccafb8592de738a455a9192d293a2b0c7c008d
Glenn Sequeira <glenn@sequeira.io>
2020-10-08 00:59:39 +0300

    Chaos Experiment #3

--- chaos-experiments/experiment1.sh
+++ chaos-experiments/experiment1.sh
@@ -7,4 +7,4 @@ GREMLIN_API_KEY=$2
 CIRCLE_USERNAME=$3
 
 
-curl -i -X POST "https://api.gremlin.com/v1/attacks/new?teamId=${GREMLIN_TEAM_ID}" -H 'Content-Type: application/json;charset=utf-8' -H "Authorization: Key ${GREMLIN_API_KEY}" -d '{"target":{"type":"Random","containers":{"multiSelectLabels":{"io.kubernetes.container.name":["'"$CIRCLE_USERNAME-cicd-chaos-app"'"]}},"percent":100},"command":{"type":"cpu","commandType":"CPU","args":["-l","120","-p","20","-a"]}}'
+curl -i -X POST 'https://api.gremlin.com/v1/attacks/new?teamId=a03d53c4-a331-5cc9-a94b-1815aba199f5' -H 'Content-Type: application/json;charset=utf-8' -H 'Authorization: Bearer Yy0zODZkNjNhMS1kMGI1LTU2ODYtYTdkZi1hMjA2OTRiYTBlNmI6Y29tbXVuaXR5K2NpcmNsZUBncmVtbGluLmNvbToxYzA5MWZhYS04NTdlLTQzMzEtODkxZi1hYTg1N2UzMzMxOGQ=' -d '{"target":{"containers":{"ids":["d95ae0dce94c87e224cb7748e3175dc0384edaf734017c7d65a9818b42dbaf1e"]},"type":"Exact"},"command":{"type":"latency","commandType":"Latency","args":["-l","300","-h","^api.gremlin.com","-p","^53","-m","600"],"providers":[]}}'
